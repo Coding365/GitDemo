261c7a68cdf571abdda9d25603cdd156199f803c
Mac <Mac@MacdeiMac.local>
2015-04-14 04:10:24 +0800

    dddd

---
+++ GitDemo/NewObject.h
@@ -0,0 +1,13 @@
+//
+//  NewObject.h
+//  GitDemo
+//
+//  Created by Mac on 15/4/14.
+//  Copyright (c) 2015年 wuyx. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewObject : NSObject
+
+@end
---
+++ GitDemo/NewObject.m
@@ -0,0 +1,13 @@
+//
+//  NewObject.m
+//  GitDemo
+//
+//  Created by Mac on 15/4/14.
+//  Copyright (c) 2015年 wuyx. All rights reserved.
+//
+
+#import "NewObject.h"
+
+@implementation NewObject
+
+@end
